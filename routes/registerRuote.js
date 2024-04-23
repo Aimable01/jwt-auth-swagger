e50cfac10015499526f926c934acf859830cd797
unknown <aimable.kwizera14@gmail.com>
2024-04-23 19:18:40 +0200

    register route

---
+++ routes/registerRuote.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const register = require("../controllers/register.js");
+
+const registerRouter = express.Router();
+
+registerRouter.post("/", register);
+
+module.exports = registerRouter;
