eb52a87aa506ce98468daa3e53c9974a72053d16
unknown <aimable.kwizera14@gmail.com>
2024-04-23 20:46:31 +0200

    the entry file

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-import express from "express";
-import mongoose from "mongoose";
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const registerRouter = require("./routes/registerRuote.js");
+const loginRouter = require("./routes/loginRoute.js");
+const tokenCheck = require("./middleware/tokenCheck.js");
 
 const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+
+// route endpoint
+app.use("/auth/register", registerRouter);
+app.use("/auth/login", loginRouter);
+
+// protected route
+app.get("/", tokenCheck, (req, res) => {
+  res.send("You successfully logged in. Welcome to the dashboard");
+});
 
 // mongodb connection
 mongoose
