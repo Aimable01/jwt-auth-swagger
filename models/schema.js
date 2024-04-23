9d0a261d544e10e357f57952c0be357d6f37891f
unknown <aimable.kwizera14@gmail.com>
2024-04-23 18:41:47 +0200

    the user schema

---
+++ models/schema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", usersSchema);
+module.exports = User;
