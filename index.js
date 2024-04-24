d5ea1e98d645cab4a637d6beeab79b6def7465e9
unknown <aimable.kwizera14@gmail.com>
2024-04-24 18:44:59 +0200

    entry file

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const registerRouter = require("./routes/registerRuote.js");
 const loginRouter = require("./routes/loginRoute.js");
 const tokenCheck = require("./middleware/tokenCheck.js");
 
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const options = require("./swagger");
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
 
 // route endpoint
 app.use("/auth/register", registerRouter);
 app.use("/auth/login", loginRouter);
 
 // protected route
-app.get("/", tokenCheck, (req, res) => {
+app.get("/dashboard", tokenCheck, (req, res) => {
   res.send("You successfully logged in. Welcome to the dashboard");
 });
+
+app.get("/me", tokenCheck, (req, res) => {
+  res.send(req.user);
+});
+
+const spacs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spacs));
 
 // mongodb connection
 mongoose
@@ -28,3 +41,38 @@ mongoose
   .catch((e) => {
     console.log(`An error in connecting to mongodb: ${e}`);
   });
+
+/**
+ * @swagger
+ *   /dashboard:
+ *     get:
+ *       summary: get home
+ *       description: Access protected dashboard
+ *       security:
+ *         - BearerAuth: []
+ *       responses:
+ *         '200':
+ *           description: Success response
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *         '401':
+ *           description: Unauthorized
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *         '404':
+ *           description: Not found
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *         '500':
+ *           description: Internal server error
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ */
