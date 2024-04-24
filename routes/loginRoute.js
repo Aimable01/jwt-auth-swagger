6008c071ee9c535019c3320cdc14b968097ef907
unknown <aimable.kwizera14@gmail.com>
2024-04-24 18:44:45 +0200

    login register routes

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -6,3 +6,32 @@ const loginRouter = express.Router();
 loginRouter.post("", login);
 
 module.exports = loginRouter;
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: The users authentication api
+ */
+
+/**
+ * @swagger
+ *   /auth/login:
+ *     post:
+ *       summary: login
+ *       tags: [User]
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       responses:
+ *         200:
+ *           description: Login success
+ *           content:
+ *             application/json:
+ *               type: object
+ *         500:
+ *           description: Internal server error
+ */
--- routes/registerRuote.js
+++ routes/registerRuote.js
@@ -6,3 +6,25 @@ const registerRouter = express.Router();
 registerRouter.post("/", register);
 
 module.exports = registerRouter;
+
+/**
+ * @swagger
+ *   /auth/register:
+ *     post:
+ *       summary: user register
+ *       tags: [User]
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       responses:
+ *         200:
+ *           description: registration success
+ *           content:
+ *             application/json:
+ *               type: object
+ *         500:
+ *           description: Internal server error
+ */
